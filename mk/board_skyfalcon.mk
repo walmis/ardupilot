8b599ea0cf5fc4b6023c9164e672450048c91f6d
Valmantas Paliksa <walmis@gmail.com>
2018-02-28 16:15:45 +0200

    Enable lto archive build

--- mk/board_skyfalcon.mk
+++ mk/board_skyfalcon.mk
@@ -29,8 +29,8 @@ WARNFLAGSCXX    = \
 
 DEPFLAGS        =   -MD -MP -MT $@
 
-CXXOPTS         =   -ffunction-sections -fdata-sections -fno-exceptions -fsigned-char -fno-rtti
-COPTS           =   -ffunction-sections -fdata-sections -fsigned-char
+CXXOPTS         =   -ffunction-sections -fdata-sections -fno-exceptions -fsigned-char -fno-rtti -flto
+COPTS           =   -ffunction-sections -fdata-sections -fsigned-char -flto
 
 ASOPTS          =   -x assembler-with-cpp 
 
